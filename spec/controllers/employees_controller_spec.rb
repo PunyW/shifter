45862074eb21f4d6ca86983fa41f3f5289ab0757
puny <joel.jarvinen@cs.helsinki.fi>
2015-04-26 19:50:44 +0300

    Fixed employee controller spec

--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -18,61 +18,24 @@ RSpec.describe EmployeesController, type: :controller do
           Employee.create!(first_name: 'Kalam', last_name: 'Mekhar', work_percent: 1)
           Employee.create!(first_name: 'Ben', last_name: 'Quick', work_percent: 1)
           Employee.create!(first_name: 'Ganoes', last_name: 'Paron', work_percent: 1)
+          xhr :get, :index, format: :json
         end
 
-        describe 'without keywords' do
-          before do
-            xhr :get, :index, format: :json
+        subject(:results) { JSON.parse(response.body) }
+
+        context 'should show all the employees' do
+          it 'should 200' do
+            expect(response.status).to eq 200
           end
 
-          subject(:results) { JSON.parse(response.body) }
-
-          context 'should show all the employees' do
-            it 'should 200' do
-              expect(response.status).to eq 200
-            end
-
-            it 'should return 3 results' do
-              expect(results.size).to eq 3
-            end
-
-            it "should include 'Kalam', 'Ben' and 'Ganoes'" do
-              expect(results.map(&extract_name)).to include('Kalam')
-              expect(results.map(&extract_name)).to include('Ben')
-              expect(results.map(&extract_name)).to include('Ganoes')
-            end
-          end
-        end
-
-        describe 'with keywords' do
-          before do
-            xhr :get, :index, format: :json, keywords: keywords
+          it 'should return 3 results' do
+            expect(results.size).to eq 3
           end
 
-          subject(:results) { JSON.parse(response.body) }
-
-          context 'when the search finds results' do
-            let(:keywords) { 'kalam' }
-
-            it 'should 200' do
-              expect(response.status).to eq 200
-            end
-
-            it 'should return one result' do
-              expect(results.size).to eq 1
-            end
-
-            it "should include 'Kalam'" do
-              expect(results.map(&extract_name)).to include('Kalam')
-            end
-          end
-
-          context "when the search doesn't find results" do
-            let(:keywords) { 'pekka' }
-
-            it 'should return no results' do
-              expect(results.size).to eq 0
-            end
+          it "should include 'Kalam', 'Ben' and 'Ganoes'" do
+            expect(results.map(&extract_name)).to include('Kalam')
+            expect(results.map(&extract_name)).to include('Ben')
+            expect(results.map(&extract_name)).to include('Ganoes')
           end
         end
       end
