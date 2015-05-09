a14c809578c988a09cf2ca06aabb19be8d8eb489
puny <joel.jarvinen@cs.helsinki.fi>
2015-05-09 22:19:01 +0300

    update

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,13 +3,17 @@ var app = angular.module('ghPages', [
 ]);
 
 app.config(['$routeProvider',
-    function($routeProvider) {
+    function ($routeProvider) {
         $routeProvider.when('/', {
                 templateUrl: 'partials/index.html',
                 controller: 'MainCtrl'
             }
         ).when('/hours', {
                 templateUrl: 'partials/hours.html',
+                controller: 'MainCtrl'
+            }
+        ).when('/docs', {
+                templateUrl: 'partials/docs.html',
                 controller: 'MainCtrl'
             }
         ).otherwise({
@@ -19,71 +23,75 @@ app.config(['$routeProvider',
     }
 ]);
 
-app.controller('MainCtrl', function($scope) {
+app.controller('MainCtrl', function ($scope) {
     $scope.hours = [
-        {
-            date: '09.03.2015',
-            hours: 4
-        },
-        {
-            date: '10.03.2015',
-            hours: 3
-        },
-        {
-            date: '11.03.2015',
-            hours: 4
-        },
-        {
-            date: '14.03.2015',
-            hours: 2
-        },
-        {
-            date: '18.03.2015',
-            hours: 2.5
-        },
-        {
-            date: '21.03.2015',
-            hours: 2.5
-        },
-        {
-            date: '23.03.2015',
-            hours: 4
-        },
-        {
-            date: '31.03.2015',
-            hours: 2.75
-        },
-        {
-            date: '02.04.2015',
-            hours: 1.5
-        },
-        {
-            date: '19.04.2015',
-            hours: 3.5
-        },
-        {
-            date: '22.04.2015',
-            hours: 2
-        },
-        {
-            date: '25.04.2015',
-            hours: 0.5
-        },
-        {
-            date: '26.04.2015',
-            hours: 3.5
-        },
-        {
-            date: '27.04.2015',
-            hours: 6.75
-        },
-        {
-            date: '08.05.2015',
-            hours: 1.15
-        },
-        {
-            date: '09.05.2015',
-            hours: 3
-        }
+        [
+            {
+                date: '09.03.2015',
+                hours: 4
+            },
+            {
+                date: '10.03.2015',
+                hours: 3
+            },
+            {
+                date: '11.03.2015',
+                hours: 4
+            },
+            {
+                date: '14.03.2015',
+                hours: 2
+            },
+            {
+                date: '18.03.2015',
+                hours: 2.5
+            },
+            {
+                date: '21.03.2015',
+                hours: 2.5
+            },
+            {
+                date: '23.03.2015',
+                hours: 4
+            },
+            {
+                date: '31.03.2015',
+                hours: 2.75
+            }
+        ], [
+            {
+                date: '02.04.2015',
+                hours: 1.5
+            },
+            {
+                date: '19.04.2015',
+                hours: 3.5
+            },
+            {
+                date: '22.04.2015',
+                hours: 2
+            },
+            {
+                date: '25.04.2015',
+                hours: 0.5
+            },
+            {
+                date: '26.04.2015',
+                hours: 3.5
+            },
+            {
+                date: '27.04.2015',
+                hours: 6.75
+            }
+        ], [
+            {
+                date: '08.05.2015',
+                hours: 1.15
+            },
+            {
+                date: '09.05.2015',
+                hours: 3
+            }
+        ]
     ]
 });
