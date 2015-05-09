359be2f1ddac0491ade7b47b1b2782d8dddb684b
Joel Järvinen <PunyW@users.noreply.github.com>
2015-05-09 21:54:11 +0300

    Update hours

--- javascripts/app.js
+++ javascripts/app.js
@@ -32,6 +32,58 @@ app.controller('MainCtrl', function($scope) {
         {
             date: '11.03.2015',
             hours: 4
+        },
+        {
+            date: '14.03.2015',
+            hours: 2
+        },
+        {
+            date: '18.03.2015',
+            hours: 2.5
+        },
+        {
+            date: '21.03.2015',
+            hours: 2.5
+        },
+        {
+            date: '23.03.2015',
+            hours: 4
+        },
+        {
+            date: '31.03.2015',
+            hours: 2.75
+        },
+        {
+            date: '02.04.2015',
+            hours: 1.5
+        },
+        {
+            date: '19.04.2015',
+            hours: 3.5
+        },
+        {
+            date: '22.04.2015',
+            hours: 2
+        },
+        {
+            date: '25.04.2015',
+            hours: 0.5
+        },
+        {
+            date: '26.04.2015',
+            hours: 3.5
+        },
+        {
+            date: '27.04.2015',
+            hours: 6.75
+        },
+        {
+            date: '08.05.2015',
+            hours: 1.15
+        },
+        {
+            date: '09.05.2015',
+            hours: 3
         }
     ]
 });
